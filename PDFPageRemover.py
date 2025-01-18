177622b19948fb967a6978a2bab703ab466d267d
maazsyed <smaaz015@gmail.com>
2025-01-18 14:00:47 +0100

    PDF page removal script.

---
+++ PDFPageRemover.py
@@ -0,0 +1,26 @@
+import PyPDF2
+
+def remove_page(input_pdf, output_pdf, page_number):
+    # Open the PDF file
+    with open(input_pdf, 'rb') as infile:
+        reader = PyPDF2.PdfReader(infile)
+        writer = PyPDF2.PdfWriter()
+
+        # Loop through all the pages and add them to the writer, except the page to be removed
+        for i in range(len(reader.pages)):
+            if i != page_number:
+                writer.add_page(reader.pages[i])
+
+        # Write the output PDF
+        with open(output_pdf, 'wb') as outfile:
+            writer.write(outfile)
+
+# Example usage to remove the last page
+input_pdf = 'D:\\Downloads\\Huda & Adil.pdf'
+output_pdf = 'D:\\Downloads\\Huda & Adil - updated.pdf'
+
+with open(input_pdf, 'rb') as infile:
+    reader = PyPDF2.PdfReader(infile)
+    num_pages = len(reader.pages)
+
+remove_page(input_pdf, output_pdf, num_pages - 1)  # This removes the last page
